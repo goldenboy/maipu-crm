835225c9ff1b3b2562973c40350c36baa1fc48bd
Luis Barrueco <luis.barrueco@gmail.com>
2009-09-21 20:23:57 -0300

    Agrego el script de importación de sucursales.

---
+++ importar_sucursales.py
@@ -0,0 +1,35 @@
+import sugar
+import crm_config
+import sys
+
+
+# Me conecto a la instancia de SugarCRM.
+instancia = sugar.InstanciaSugar(crm_config.WSDL_URL, crm_config.USUARIO,
+                    crm_config.CLAVE, ['mm002_Sucursales'])
+
+# La lista 'campos' tiene los nombres de las columnas ordenadas correctamente.
+columnas = ['sucursales_codigo', 'sucursales_descripcion']
+
+# Leo el archivo de datos.
+arch_datos = open('CRM-Sucursales.txt')
+datos = arch_datos.readlines()
+
+
+for linea in datos:
+    campos = [cadena.rstrip().lstrip() for cadena in linea.split(';')]
+    
+    # Creo un objeto nuevo del modulo Sucursales.
+    objeto = sugar.ObjetoSugar(instancia.modulos['mm002_Sucursales'])
+    for i in range(len(columnas)):
+        objeto.importar_campo(columnas[i], unicode(campos[i], 'iso-8859-1'))
+#        print unicode(campos[i], 'iso-8859-1')
+    objeto.grabar()
+    print "1 objeto grabado."
+    
+
+print "Fin."
+
+
+
+
+
