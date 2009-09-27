109b54ba7296dfa38835b34e9c6f880360362a81
Luis Barrueco <luis.barrueco@gmail.com>
2009-09-26 22:59:34 -0300

    El script de importación de empleados ahora asigna el campo 'name'

--- importar_empleados.py
+++ importar_empleados.py
@@ -22,6 +22,8 @@ for linea in datos:
     objeto = sugar.ObjetoSugar(instancia.modulos['mm002_Empleados'])
     for i in range(len(columnas)):
         objeto.importar_campo(columnas[i], unicode(campos[i], 'iso-8859-1'))
+        if i == 1:
+            objeto.importar_campo('name', unicode(campos[i], 'iso-8859-1'))
 #        print unicode(campos[i], 'iso-8859-1')
     objeto.grabar()
     print "1 objeto grabado."
