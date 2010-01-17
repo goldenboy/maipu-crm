b0551363a1b83bf7d1add55463d38580d466b557
Luis Barrueco <luis.barrueco@gmail.com>
2010-01-16 21:26:01 -0300

    Agrego el codigo del monitor al repositorio, primera version.
    Viene con el directorio de plugins incluido

---
+++ monitor/monitor_config.py
@@ -0,0 +1,15 @@
+
+# Configuracion general
+
+DIR_BASE = '/srv/CRM/procesar'
+PLUGIN_DIRS = ['plugins/']
+
+
+
+# Configuracion de los scripts a invocar segun el path del archivo importado
+
+PATHS = [('/venta/', 'importar_venta'),
+]
+
+
+
