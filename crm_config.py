64f1881bce626d2e5e899afb61bc90634a607931
Luis Barrueco <luis.barrueco@gmail.com>
2010-04-10 19:29:41 -0300

    Agrego sugarcrm.amaipu.com.ar a la lista de servers

--- crm_config.py
+++ crm_config.py
@@ -1,6 +1,7 @@
 
 #WSDL_URL = 'http://localhost/sugar_new/soap.php?wsdl'
-WSDL_URL = 'http://sugardev.amaipu.com.ar/soap.php?wsdl'
+#WSDL_URL = 'http://sugardev.amaipu.com.ar/soap.php?wsdl'
+WSDL_URL = 'http://sugarcrm.amaipu.com.ar/soap.php?wsdl'
 #WSDL_URL = 'http://sugardemo.amaipu.com.ar/soap.php?wsdl'
 USUARIO = 'sugarcrm'
 CLAVE = 'aLech9hi'
