75ddcc681cd726507fe3550bd69a92a23340a949
Luis Barrueco <luis.barrueco@gmail.com>
2010-07-12 20:45:06 -0300

    Modifico script de importacion de items (era accesorios)
    Agrego soporte para tarjetas y seguros.
    Distingo por tipo de módulo para usar

--- historico/importar_item_historico.py
+++ historico/importar_item_historico.py
@@ -14,7 +14,7 @@ currency_id = '9f4c22ed-f82e-9aa2-5f77-4c28f762e851'
 
 # Configuro el logging
 logging.basicConfig(level=monitor_config.LOG_LEVELS[monitor_config.LOG_LEVEL])
-logger = logging.getLogger("accesorio_historico")
+logger = logging.getLogger("item_historico")
 
 
 def procesar(instancia, pathname):
@@ -40,7 +40,7 @@ def procesar(instancia, pathname):
         if(not ret):
             # Si hubo un error al procesar la linea del archivo
             if(not hubo_errores):
-                errores = open('accesorio_historico-' + err_filename, 'w')
+                errores = open('item_historico-' + err_filename, 'w')
             errores.write(linea)
             errores.flush()
             hubo_errores = True
@@ -59,23 +59,38 @@ def procesar_linea(instancia, linea):
     
     datos = linea.split(';')
 
-    # Para cada accesorio a importar, hago una busqueda de accesorios por 
-    # numero_factura, por si los accesorios fueron ingresados previamente
-    busq = instancia.modulos['mm002_Accesorios'].buscar(numero_orden=datos[0])
+    # [3] => Seguros
+    # [2] => Tarjetas
+    # [1] => Accesorios
+    
+    if datos[6] == '3':
+        modulo_item = 'mm002_Seguros'
+        tuplas = [('numero_orden', datos[0]), ('cliente_id', datos[1]),
+                    ('fecha_facturacion', datos[2]), ('description', datos[4]),
+                    ('importe', datos[5])]
+    elif datos[6] == '2':
+        modulo_item = 'mm002_Tarjetas'
+        tuplas = [('numero_orden', datos[0]), ('cliente_id', datos[1]),
+                    ('fecha_facturacion', datos[2]), ('description', datos[4])]
+    else:
+        modulo_item = 'mm002_Accesorios'
+        tuplas = [('numero_orden', datos[0]), ('cliente_id', datos[1]),
+                    ('fecha_facturacion', datos[2]), ('marca_codigo', datos[3]),
+                    ('description', datos[4]), ('importe', datos[5])]
+    
+    # Para cada item a importar, hago una busqueda por 
+    # numero_orden, por si los items fueron ingresados previamente
+    busq = instancia.modulos[modulo_item].buscar(numero_orden=datos[0])
     if len(busq) != 0:
         # si hay algun resultado, uso el primero
         objeto = busq[0]
     else:
-        # Creo un objeto nuevo del modulo Turnos.
-        objeto = sugar.ObjetoSugar(instancia.modulos['mm002_Accesorios'])
-
-    # Defino la plantilla con los campos.
-    campos = ['numero_orden', 'cliente_id', 'fecha_facturacion', 'marca_codigo',
-            'accesorio_descripcion_c', 'importe', 'accesorio_tipo_c']
+        # Creo un objeto nuevo del modulo del item.
+        objeto = sugar.ObjetoSugar(instancia.modulos[modulo_item])
 
 
     # Cargo todos los valores importados en el objeto que entrara en sugar.
-    for campo in zip(campos, datos):
+    for campo in tuplas:
         if campo[0].startswith('fecha') and campo[1] == '00000000':
             # No importo este campo, lo dejo en blanco
             pass
@@ -88,8 +103,6 @@ def procesar_linea(instancia, linea):
 
     # Verifico que todos los contactos referenciados existan en Sugar y sean
     # unicos. En caso de que no sean unicos, uso el primero?.
-
-    # Verifico que exista el cliente.
     
     logger.debug("Buscando cliente")
     valor = objeto.obtener_campo('cliente_id').a_sugar()
@@ -109,33 +122,34 @@ def procesar_linea(instancia, linea):
     
 
     # Luego veo que la marca este cargada, y si lo esta, agrego la descripcion
-    # al accesorio.
-    valor = objeto.obtener_campo('marca_codigo').a_sugar()
-    res = instancia.modulos['mm002_Marcas'].buscar(marcas_codigo=valor)
-    if len(res) > 0:
-        # Agarro la primera marca con ese codigo
-        marca = res[0]
+    # al accesorio (si el objeto es un accesorio).
+    if modulo_item == 'mm002_Accesorios':
+        valor = objeto.obtener_campo('marca_codigo').a_sugar()
+        res = instancia.modulos['mm002_Marcas'].buscar(marcas_codigo=valor)
+        if len(res) > 0:
+            # Agarro la primera marca con ese codigo
+            marca = res[0]
         
-        marca_descripcion = marca.obtener_campo('marcas_descripcion').a_sugar()
+            marca_descripcion = marca.obtener_campo('marcas_descripcion').a_sugar()
         
-        # Cargo la descripcion de la marca en el accesorio
-        objeto.importar_campo('marca_descripcion', unicode(marca_descripcion))
+            # Cargo la descripcion de la marca en el accesorio
+            objeto.importar_campo('marca_descripcion', unicode(marca_descripcion))
 
 
-    # Voy a darle un valor al campo 'name', utilizando la descripcion del acc.
-    logger.debug("Dando nombre al accesorio.")
-    nombre = objeto.obtener_campo('accesorio_descripcion_c').a_sugar()
+    # Voy a darle un valor al campo 'name', utilizando la descripcion del item.
+    logger.debug("Dando nombre al item.")
+    nombre = objeto.obtener_campo('description').a_sugar()
     objeto.importar_campo('name', unicode(nombre))
 
 
 
     # Aqui ya estan creadas todas las entradas en Sugar de las cuales este
-    # accesorio depende. Ya puedo agrear el accesorio a la base de datos.
-
-    objeto.importar_campo('currency_id', currency_id)
+    # item depende. Ya puedo agrear el item a la base de datos.
+    if modulo_item != 'mm002_Tarjetas':
+        objeto.importar_campo('currency_id', currency_id)
     logger.debug("Objeto listo.")
 
-    logger.debug("Grabando un nuevo ACCESORIO...")
+    logger.debug("Grabando un nuevo ITEM...")
     logger.debug(objeto.grabar())
 
     if existe_cliente:
@@ -148,8 +162,8 @@ def procesar_linea(instancia, linea):
 def obtener_instancia():
     # Me conecto a la instancia de SugarCRM.
     instancia = sugar.InstanciaSugar(crm_config.WSDL_URL, crm_config.USUARIO,
-                    crm_config.CLAVE, ['mm002_Accesorios', 'mm002_Marcas',
-                                        'Contacts'],
+                    crm_config.CLAVE, ['mm002_Accesorios', 'mm002_Tarjetas',
+                                'mm002_Seguros', 'mm002_Marcas', 'Contacts'],
                     crm_config.LDAP_KEY, crm_config.LDAP_IV)
 
     return instancia
